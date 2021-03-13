8239c5af3d56615076805bc625c2c9f563279f2f
EC2 Default User <ec2-user@ip-172-31-4-208.ec2.internal>
2021-03-13 08:55:34 +0000

    [Finish]2-8

---
+++ sample_app/db/migrate/20210309091627_create_lists.rb
@@ -0,0 +1,11 @@
+class CreateLists < ActiveRecord::Migration[5.0]
+  def change
+    create_table :lists do |t|
+
+      t.string :title
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
